fa335ccad71a0a3446988cab33ccc6513abb133c
Noman <111453298+Noman0010@users.noreply.github.com>
2023-03-13 00:05:27 +0600

    Add files via upload

---
+++ (Lec-5)Fibonacci.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+ int n;
+ scanf("%d",&n);
+
+ int a = 0;
+ int b = 1;
+ printf("%d ",a);
+ for(int i=1; i<=n; i++){
+    int fibo = a+b;
+    printf("%d ",fibo);
+    a=b;
+    b=fibo;
+ }
+    
+
+    return 0;
+}
