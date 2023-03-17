7300b289f87e6f7e0a3dcbe004a22850e9275116
Noman <111453298+Noman0010@users.noreply.github.com>
2023-03-18 00:59:25 +0600

    Update 2dio.cpp

--- 2dio.cpp
+++ 2dio.cpp
@@ -4,14 +4,12 @@ int main()
 {
     int arr[4][3];
 
-    printf("Enter array elements:\n");
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 3; j++) {
             scanf("%d", &arr[i][j]);
         }
     }
 
-    printf("\nArray elements are:\n");
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 3; j++) {
             printf("%d ", arr[i][j]);
